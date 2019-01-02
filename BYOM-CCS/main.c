6ac2de383beb116a8fc7bdf0745b7b054c9d2270
Bill Hunt <william.hunt@northwestern.edu>
2019-01-02 11:48:38 -0600

    Added two more comments to verify git function

--- BYOM-CCS/main.c
+++ BYOM-CCS/main.c
@@ -6,5 +6,7 @@
 int main(void)
 {
     // Test comment 1 - added to verify CCS github functionality
+    // Test comment 2 - added to verify CCS github functionality
+    // Test comment 3 - added to verify CCS github functionality
 	return 0;
 }
